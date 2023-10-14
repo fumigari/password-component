8014e452b0977fc12bd066047a8c1addd96b53b1
Ariany Ferreira <arianysferreira1@gmail.com>
2023-10-14 18:28:58 -0300

    chore: config files

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   verbose: true,
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>./src/components/requirement/index.tsx",
+  }
 };
