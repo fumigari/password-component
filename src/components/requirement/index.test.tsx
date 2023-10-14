222ab6ad421bf1beaaf1a7d2be37e6e4e763c37e
Ariany Ferreira <arianysferreira1@gmail.com>
2023-10-14 18:20:33 -0300

    refactor: add id on requirement component

--- src/components/requirement/index.test.tsx
+++ src/components/requirement/index.test.tsx
@@ -4,7 +4,7 @@ import Requirement from '.';
 
 describe("<Requirement />", () => {
 	test("Should render Requirement", () => {
-		render(<Requirement text="test" changeIcon={false}/>);
+		render(<Requirement id="1" text="test" changeIcon={false}/>);
 		const iconRequirement = screen.queryByTitle("xmark");
 		const textRequirement = screen.queryByText("test");
 		expect(iconRequirement).toBeInTheDocument();
